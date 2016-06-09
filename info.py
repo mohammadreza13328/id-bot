b49163c293bcda17b19e8bd161e4fc89a2a2ad65
Negative <negative_amir@yahoo.com>
2016-06-09 12:31:03 +0430

    Update info.py

--- info.py
+++ info.py
@@ -3,12 +3,16 @@
 #info bot created by negative
 #info bot created by negative
 import telebot
+from telebot import types
+import requests
+import simplejson as json
 import sys
 import json
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-bot = telebot.TeleBot("TOKEN")
+bot = telebot.TeleBot("205751413:AAHVcAOpkTQ5VFeMhM6ga8nvRTnWfbML3NA")
+API_PATH = 'http://api.vajehyab.com/v2/public'
 
 @bot.message_handler(commands=['start', 'help'])
 def welcome(m):
@@ -44,6 +48,13 @@ def p(m):
     bot.send_message(uid, "Taylor Team development Telegram bot and web mastering \n\n developers : \n [negative](https://telegram.me/negative_officiall) \n [Parham](https://telegram.me/UnFriendlly)", parse_mode="Markdown")
     bot.send_photo(uid, open('taylor.jpg'), caption="@Taylor_Team")
 
+@bot.message_handler(func=lambda m: m.chat.type == 'private', commands=['idme'])
+def test_handler(m):
+    cid = m.chat.id
+    fl = m.chat.first_name
+    bot.send_message(cid, "*{}*  Your ID = ```{}```".format(fl,cid), parse_mode="Markdown")
+    
+
 @bot.message_handler(func=lambda message: True)  # on pv all pm / group /id /me pm created by negative
 def id(m):      # info menu
     cid = m.chat.id #id from
@@ -59,6 +70,7 @@ def id(m):      # info menu
 #info text  by negative , taylor team copy right
     bot.send_chat_action(cid, "typing")
     bot.reply_to(m, "*ID from* : ```{}``` \n\n *Chat name* : ```{}``` \n\n\n *Your Username* : ```{}``` \n\n *Your First Name* : ```{}```\n\n *Your Last Name* : ```{}```\n\n *Type From* : ```{}``` \n\n *Msg data* : ```{}```\n\n *Your Msg* : ```{}```\n\n* pind msg * : ```{}```\n\n *from* : ```{}```".format(cid,title,usr,f,l,t,d,text,p,fromm), parse_mode="Markdown")
+    
 
 
 bot.polling()
